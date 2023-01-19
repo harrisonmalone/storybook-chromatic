34b8bd350af2fba7895298062bec8c35eb158ee9
Harrison Malone <harrison.malone@hey.com>
2023-01-19 11:29:16 +1100

    Update button border

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,5 +3,5 @@ type ButtonProps = {
 }
 
 export const Button = ({ buttonText }: ButtonProps) => {
-  return <button className="border-4 p-1 border-blue-500 my-2">{buttonText}</button>
+  return <button className="border-4 p-1 border-red-500 my-2">{buttonText}</button>
 }
